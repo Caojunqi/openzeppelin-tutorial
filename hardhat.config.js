910435e60cacd0012447fea9a53b6e702000d37b
caojunqi <caojunqi@foxmail.com>
2022-06-15 10:57:52 +0800

    Complete OpenZeppelin Learning Tutorial.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
+require('@nomiclabs/hardhat-ethers');
+require("@nomiclabs/hardhat-truffle5");
+require('@openzeppelin/hardhat-upgrades');
+
+
+const { alchemyApiKey, mnemonic } = require('./secrets.json');
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.7.3",
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${alchemyApiKey}`,
+      accounts: { mnemonic: mnemonic },
+    },
+  },
 };
